2309a9d0dad59847e68d28d4149aec350013df9b
Sarah Phillips <snphillips@gmail.com>
2020-09-18 09:01:33 -0400

    add heroku connection

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const db = require('./queries')
 const port = 3001
 
 
+
 const { DATABASE_URL } = process.env;
 
 
@@ -32,6 +33,39 @@ app.get('/players/:id', db.getPlayerById)
 app.post('/players', db.createPlayer)
 app.put('/players/:id', db.updatePlayer)
 app.delete('/players/:score', db.deletePlayer)
+
+
+
+        // Heroku
+        // https://devcenter.heroku.com/articles/heroku-postgresql#connecting-in-node-js
+        // *****************
+        const { Client } = require('pg');
+
+        const client = new Client({
+          connectionString: process.env.DATABASE_URL,
+          // ssl: true,
+        });
+
+        client.connect();
+
+        client.query('SELECT * FROM leaderboard LIMIT 10;', (err, res) => {
+          if (err) throw err;
+          for (let row of res.rows) {
+            console.log(JSON.stringify(row));
+          }
+          client.end();
+        });
+        // *****************
+
+
+
+
+
+
+
+
+
+
 
 // **********************************
 // app.uses
