7c428f13fece32c3eb5c7083966ddeec4c4dd608
Sarah Phillips <snphillips@gmail.com>
2020-09-18 18:06:13 -0400

    make it better

--- queries.js
+++ queries.js
@@ -10,26 +10,26 @@ const pool = new Pool({
   port: process.env.DB_PORT || 5432
 })
 
-        // Heroku
-        // https://devcenter.heroku.com/articles/heroku-postgresql#connecting-in-node-js
-        // *****************
-        const { Client } = require('pg');
+        // // Heroku
+        // // https://devcenter.heroku.com/articles/heroku-postgresql#connecting-in-node-js
+        // // *****************
+        // const { Client } = require('pg');
 
-        const client = new Client({
-          connectionString: process.env.DATABASE_URL,
-          // ssl: true,
-        });
+        // const client = new Client({
+        //   connectionString: process.env.DATABASE_URL,
+        //   // ssl: true,
+        // });
 
-        client.connect();
+        // client.connect();
 
-        client.query('SELECT * FROM leaderboard LIMIT 10;', (err, res) => {
-          if (err) throw err;
-          for (let row of res.rows) {
-            console.log(JSON.stringify(row));
-          }
-          client.end();
-        });
-        // *****************
+        // client.query('SELECT * FROM leaderboard LIMIT 10;', (err, res) => {
+        //   if (err) throw err;
+        //   for (let row of res.rows) {
+        //     console.log(JSON.stringify(row));
+        //   }
+        //   client.end();
+        // });
+        // // *****************
 
 
 const getPlayers = (request, response) => {
