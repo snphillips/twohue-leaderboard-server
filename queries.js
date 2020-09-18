0c304667dec5a9c8391a48eceed5c5ce07803b58
Sarah Phillips <snphillips@gmail.com>
2020-09-18 18:01:32 -0400

    make it better

--- queries.js
+++ queries.js
@@ -10,6 +10,27 @@ const pool = new Pool({
   port: process.env.DB_PORT || 5432
 })
 
+        // Heroku
+        // https://devcenter.heroku.com/articles/heroku-postgresql#connecting-in-node-js
+        // *****************
+        const { Client } = require('pg');
+
+        const client = new Client({
+          connectionString: process.env.DATABASE_URL,
+          // ssl: true,
+        });
+
+        client.connect();
+
+        client.query('SELECT * FROM leaderboard LIMIT 10;', (err, res) => {
+          if (err) throw err;
+          for (let row of res.rows) {
+            console.log(JSON.stringify(row));
+          }
+          client.end();
+        });
+        // *****************
+
 
 const getPlayers = (request, response) => {
   pool.query('SELECT * FROM leaderboard ORDER BY id ASC LIMIT 10;', (error, results) => {
